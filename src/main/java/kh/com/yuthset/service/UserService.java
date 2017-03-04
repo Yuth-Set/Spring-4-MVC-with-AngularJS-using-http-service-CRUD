96b0bf6e9c923201f2d7e3816c0a6c895c00b88b
yuthset <syuth89@gmail.com>
2017-03-04 15:11:44 +0700

    first commit for spring 4 MVC with AnuglarJS using http service CRUD

---
+++ src/main/java/kh/com/yuthset/service/UserService.java
@@ -0,0 +1,17 @@
+package kh.com.yuthset.service;
+
+import java.util.List;
+
+import kh.com.yuthset.model.User;
+
+public interface UserService {
+
+	User findById(long id);
+	User findByName(String name);
+	void saveUser(User user);
+	void updateUser(User user);
+	void deleteUserById(long id);
+	List<User> findAllUsers();
+	void deleteAllUsers();
+	public boolean isUserExist(User user);
+}
